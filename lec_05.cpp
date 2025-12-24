3ceceb626743b53b022febef788614eb123d09f9
ADNAN-AB1R <168480643+ADNAN-AB1R@users.noreply.github.com>
2025-12-25 00:44:44 +0600

    25/12/2025

---
+++ lec_05.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    /*
+    // Student er 75% or above attendence thaka lagbe pass korar jonno. Ekhon 60 er upore mark pay. Then satisfactory otherwise improvement required 
+
+    double present = 80;
+    double mark = 100;
+    if (present >= 75)
+    {
+        if (mark >= 60)
+        {
+            cout << "Satisfactory";
+        }
+        else{
+            cout << "Improvement required";
+        }
+        
+    }
+    else{
+        cout << "fail";
+    }
+
+    
+
+    //Gari bridge cross korar jonno garir wt 20ton er niche hote hobe and speed 40km/h er niche hote hobe. axles 4 ta ba tar besi hote hbe.
+
+    int wt,speed,axles;
+
+    cout << "Please enter the wt(kg) of the vehicle: " ;
+    cin >> wt;
+    cout << "Please enter the speed of the vehicle: " ;
+    cin >> speed;
+    cout << "Please enter the no of axles of the vehicle: " ;
+    cin >> axles;
+
+    if ((wt/10) <= 20 && speed <= 40 && axles >=4)
+    {
+        cout << "Permission granted";
+    }
+    else{
+        cout << "Permission denied";
+    }
+    
+    
+
+    int value;
+    cout << "Please enter the value (1-7): ";
+    cin >> value;
+
+    switch (value)
+    {
+    case 1:
+        cout << "Saturday" << endl;
+        //break;
+    case 2:
+        cout << "Sunday" << endl;
+        //break;
+    case 3:
+        cout << "MOnday" << endl;
+        //break;
+    case 4:
+        cout << "Tuesday" << endl;
+        //break;
+    case 5:
+        cout << "Wednesday" << endl;
+        //break;
+    case 6:
+        cout << "Thrursday" << endl;
+        //break;
+    case 7:
+        cout << "Friday" << endl;
+        //break;
+    default:
+        cout << "Invalid input" << endl;
+       // break;
+    }
+    
+    */
+    
+    return 0;
+}
+
